6865117bcae70a5c7ebde8b854893139d3339dab
Eduardo <restini@gmail.com>
2020-01-15 16:20:21 -0300

    Date validation through model

--- app/models/applicant_profile.rb
+++ app/models/applicant_profile.rb
@@ -5,5 +5,14 @@ class ApplicantProfile < ApplicationRecord
     validates :education, presence: { message: 'não pode ficar em branco' }
     validates :applicant_description, presence: { message: 'não pode ficar em branco' }
     validates :experience, presence: { message: 'não pode ficar em branco' }
+    validate :date_of_birth_equal_today        
+
+    def date_of_birth_equal_today
+                
+        if date_of_birth == Date.current
+          errors.add(:date_of_birth, 'deve ser menor que a data de hoje.')
+        end
+    end
+
 end
   
